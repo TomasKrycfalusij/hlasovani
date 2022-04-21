c39285960d306def448d8c425b2a177df4e02211
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-04-21 11:39:28 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,63 +1,19 @@
-remote_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
 my_serial = control.device_serial_number()
-print(my_serial)
 radio.set_group(69)
+radio.set_transmit_power(7)
+radio.set_transmit_serial_number(True)
 serials = []
 moznost = 1
-#NASTAVENI MOZNOSTI HLASOVANI NA TRUE/FALSE
-hlasovani = True
-def on_button_pressed_ab():
-    global hlasovani
-    if hlasovani == True:
-        hlasovani = False
-        print(hlasovani)
-    else:
-        hlasovani = True
-        print(hlasovani)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
+votes = 1
 
 #HLASOVANI
-def on_logo_event_pressed():
-    radio.send_value("vote", moznost)
-input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
-
-
 def on_button_pressed_a():
-    global moznost
-    moznost -= 1
-    if moznost == 0:
-        moznost = 4
-    zobrazeni_hlasu()
+    global votes
+    votes = (votes+1)%4
+    basic.show_number(votes)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    global moznost
-    moznost += 1
-    if moznost == 5:
-        moznost = 1
-    zobrazeni_hlasu()
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def zobrazeni_hlasu():
-    if moznost == 1:
-        basic.show_string("A")
-    if moznost == 2:
-        basic.show_string("B")
-    if moznost == 3:
-        basic.show_string("C")
-    if moznost == 4:
-        basic.show_string("D")
-
-
-votes = [0, 0, 0, 0]
-def on_received_number(receivedNumber):
-    global votes
-    if receivedNumber == 1:
-        votes[0] += 1
-    elif receivedNumber == 2:
-        votes[1] += 1
-    elif receivedNumber == 3:
-        votes[2] += 1
-    elif receivedNumber == 4:
-        votes[3] += 1
-radio.on_received_number(on_received_number)
+def on_logo_event_pressed():
+    radio.send_value("vote", votes)
+    music.play_tone(Note.C, music.beat(100))
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
--- main.ts
+++ main.ts
@@ -1,74 +1,17 @@
-let remote_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
 let my_serial = control.deviceSerialNumber()
-console.log(my_serial)
 radio.setGroup(69)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
 let serials = []
 let moznost = 1
-// NASTAVENI MOZNOSTI HLASOVANI NA TRUE/FALSE
-let hlasovani = true
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
-    if (hlasovani == true) {
-        hlasovani = false
-        console.log(hlasovani)
-    } else {
-        hlasovani = true
-        console.log(hlasovani)
-    }
-    
-})
+let votes = 1
 // HLASOVANI
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
-    radio.sendValue("vote", moznost)
-})
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    moznost -= 1
-    if (moznost == 0) {
-        moznost = 4
-    }
-    
-    zobrazeni_hlasu()
+    votes = (votes + 1) % 4
+    basic.showNumber(votes)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    moznost += 1
-    if (moznost == 5) {
-        moznost = 1
-    }
-    
-    zobrazeni_hlasu()
-})
-function zobrazeni_hlasu() {
-    if (moznost == 1) {
-        basic.showString("A")
-    }
-    
-    if (moznost == 2) {
-        basic.showString("B")
-    }
-    
-    if (moznost == 3) {
-        basic.showString("C")
-    }
-    
-    if (moznost == 4) {
-        basic.showString("D")
-    }
-    
-}
-
-let votes = [0, 0, 0, 0]
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    
-    if (receivedNumber == 1) {
-        votes[0] += 1
-    } else if (receivedNumber == 2) {
-        votes[1] += 1
-    } else if (receivedNumber == 3) {
-        votes[2] += 1
-    } else if (receivedNumber == 4) {
-        votes[3] += 1
-    }
-    
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    radio.sendValue("vote", votes)
+    music.playTone(Note.C, music.beat(100))
 })
